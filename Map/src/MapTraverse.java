c2a2647133b8b85c8a7eed59b26fba1ada77d1fe
shiyuanliang <1779003163@qq.com>
2022-04-21 15:52:14 +0800

    Map提交_1

--- Map/src/MapTraverse.java
+++ Map/src/MapTraverse.java
@@ -1,6 +1,57 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @author LiangShiyuan
  * @create 2022-04-20 19:02
  */
 public class MapTraverse {
+
+    //宽度优先遍历
+    public  static  void  bfs(Node node){
+        if(node==null){
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        HashSet<Node> hashSet = new HashSet<>();
+        queue.add(node);
+        hashSet.add(node);
+         while(!queue.isEmpty()){
+             Node cur = queue.poll();
+             System.out.println(cur.value);
+             for(Node  next : cur.nexts){
+                 if(!hashSet.contains(next)){
+                     hashSet.add(next);
+                     queue.add(node);
+                 }
+             }
+
+        }
+    }
+
+    //深度优先遍历
+    public static  void dfs(Node node){
+        if(node==null){
+            return;
+        }
+        Stack<Node>  stack = new Stack<>();
+        HashSet<Node>  hashSet = new HashSet<>();
+        stack.add(node);
+        hashSet.add(node);
+        System.out.println(node.value);
+        while(!stack.isEmpty()){
+            Node cur = stack.pop();
+            for(Node next : cur.nexts){
+                if(!hashSet.contains(next)){
+                    stack.push(cur);
+                    stack.push(next);
+                    hashSet.add(next);
+                    System.out.println(next.value);
+                    break;
+                }
+            }
+        }
+    }
 }
---
+++ Map/src/TopoSort.java
@@ -0,0 +1,6 @@
+/**
+ * @author LiangShiyuan
+ * @create 2022-04-21 15:51
+ */
+public class TopoSort {
+}

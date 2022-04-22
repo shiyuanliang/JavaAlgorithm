5ac31967b4295bdbe5aa73276391857166d8c8f5
shiyuanliang <1779003163@qq.com>
2022-04-22 16:32:10 +0800

    Map提交_2

---
+++ Map/src/Kruskal.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+/**
+ * @author LiangShiyuan
+ * @create 2022-04-21 16:30
+ */
+public class Kruskal {
+    class Myset{
+        public HashMap<Node, List<Node>> setMap;
+
+        public  Myset(List<Node> nodes){
+            for(Node cur: nodes){
+                List<Node> set = new ArrayList<>();
+                set.add(cur);
+                setMap.put(cur, set);
+            }
+        }
+        public boolean isSameSet(Node from, Node to){
+            List<Node>  fromSet = setMap.get(from);
+            List<Node> toSet = setMap.get(to);
+            return  fromSet == toSet;
+        }
+
+        public  void union(Node from, Node to){
+            List<Node> fromSet = setMap.get(from);
+            List<Node> toSet = setMap.get(to);
+            for(Node cur: toSet){
+                fromSet.add(cur);
+                setMap.put(cur, fromSet);
+            }
+        }
+    }
+
+    public  static  class  EdgeComparator implements Comparator<Edge> {
+        @Override
+        public int compare(Edge o1, Edge o2){
+            return  o1.weight - o2.weight;
+        }
+    }
+
+    public  static Set<Edge> KruskalMst(Graph graph){
+        UnionFind unionFind= new  UnionFind(); //并查集结构
+        PriorityQueue<Edge> priorityQueue = new PriorityQueue<>(new EdgeComparator());
+        unionFind.makeSets(graph.nodes.values());
+        for(Edge edge : graph.edges){
+            priorityQueue.add(edge);
+        }
+        Set<Edge> result = new HashSet<>();
+        while(!priorityQueue.isEmpty()){
+            Edge  edge = priorityQueue.poll();
+            if(!unionFind.isSameSet(edge.from, edge.to)){
+                result.add(edge);
+                unionFind.union(edge.from, edge.to);
+            }
+        }
+        return  result;
+    }
+}
--- Map/src/TopoSort.java
+++ Map/src/TopoSort.java
@@ -1,6 +1,36 @@
+import javax.print.attribute.HashAttributeSet;
+import java.util.*;
+
 /**
  * @author LiangShiyuan
  * @create 2022-04-21 15:51
  */
 public class TopoSort {
+
+
+    //拓扑排序 主要的应用场景有编译的过程中依赖包
+    public  static List<Node>  sortTopo(Graph graph){
+        HashMap<Node, Integer>  inMap = new HashMap<>();
+        Queue<Node>  zeroInQueue = new LinkedList<>();
+        for(Node node : graph.nodes.values()){
+            inMap.put(node, node.in);
+            if(node.in ==0){
+                zeroInQueue.add(node);
+            }
+        }
+
+        //拓扑排序的结果，依次加入result
+        List<Node>  result = new ArrayList<>();
+        while (!zeroInQueue.isEmpty()){
+            Node cur  = zeroInQueue.poll();
+            result.add(cur);
+            for(Node next : cur.nexts){
+                inMap.put(next, inMap.get(next) -1);
+                if(inMap.get(next)==0){
+                    zeroInQueue.add(next);
+                }
+            }
+        }
+        return  result;
+    }
 }
